bf570b1bec5d711095218532a09be1b43d251869
Erick Simões <erick.simoes@live.com>
2021-06-28 00:41:22 -0300

    Create basic classes of Inventory

---
+++ Inventum/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    
+    List<Item> items;
+
+    void Start() {
+        
+    }
+
+}
